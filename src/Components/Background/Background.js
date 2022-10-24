a5fed0131ec02c3ef52356ab03e40b3730ee74b1
Attenea <yolanda.angarita.30@gmail.com>
2022-10-23 23:39:19 -0500

    First commit

---
+++ src/Components/Background/Background.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Background.css';
+
+function Background() {
+  return (
+    <>
+      <div className="App-header"></div>
+      <div className="App-body"></div>
+    </>
+  );
+}
+
+export default Background;
---
+++ src/Components/TabIndex/TabIndex.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TabIndex.css';
+import 'animate.css';
+
+function TabIndex() {
+  return (
+    <>
+      <div className="Container "></div>;<div className="Header ">Hola</div>;
+    </>
+  );
+}
+
+export default TabIndex;
